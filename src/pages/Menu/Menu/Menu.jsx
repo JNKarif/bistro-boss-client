cb3c38e63902d905a9fe484df90aa9de94d5f6dc
JNKarif <arif.rahman041@gmail.com>
2023-08-11 01:02:06 +0600

    menu-cover-added

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,10 @@
 import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from '../../../assets/menu/banner3.jpg'
+import pizzaBg from '../../../assets/menu/pizza-bg.jpg';
+import saladBg from '../../../assets/menu/salad-bg.jpg'
+import soupBg from '../../../assets/menu/soup-bg.jpg'
+import PopularMenu from "../../Home/PopulatMenu/PopularMenu";
 
 const Menu = () => {
   return (
@@ -6,7 +12,14 @@ const Menu = () => {
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
-      <div className="h2 text-4xl">This is menuuuuuuuu</div>
+      <Cover img={menuImg} title={"our menu"}></Cover>
+      
+      <Cover img={pizzaBg} title={"our pizza"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={saladBg} title={"our Salad"}></Cover>
+      <PopularMenu></PopularMenu>
+      <Cover img={soupBg} title={"our soup"}></Cover>
+      <PopularMenu></PopularMenu>
     </div>
   );
 };
